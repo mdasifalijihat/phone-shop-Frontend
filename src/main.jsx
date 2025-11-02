121965e06c6643871daf53ae68d85de1b999c7bb
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-02 14:00:23 +0600

    added Redux-Persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/sonner";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store"; // ✅ persistor export korbo store theke
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
-      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './userSlice';
 
-const store = configureStore({
-  reducer: {
-    user: userSlice
-  },
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userSlice from './userSlice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// ✅ create persistor
+const persistor = persistStore(store);
+
+// ✅ export both store and persistor
+export { store, persistor };
 export default store;
